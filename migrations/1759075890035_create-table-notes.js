e375096b3a12d4b3314601097fbc5622d9f8ad88
HapnanAR <34102619+hapnan@users.noreply.github.com>
2025-09-28 23:30:30 +0700

    postgres database implemented

---
+++ migrations/1759075890035_create-table-notes.js
@@ -0,0 +1,49 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable("notes", {
+    id: {
+      type: "varchar(50)",
+      primaryKey: true,
+    },
+    title: {
+      type: "text",
+      notNull: true,
+    },
+    body: {
+      type: "text",
+      notNull: true,
+    },
+    tags: {
+      type: "text[]",
+      notNull: true,
+    },
+    created_at: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    },
+    updated_at: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable("notes");
+};
